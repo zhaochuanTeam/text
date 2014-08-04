85122af13fb35676a783eb836db0b5d715e960b6
zhaochaun <307283195@qq.com>
2014-08-04 12:00:17 +0800

    创建项目

---
+++ src/com/app/githubdemo/mainActivity.java
@@ -0,0 +1,15 @@
+package com.app.githubdemo;
+import android.app.Activity;
+
+
+public class mainActivity extends Activity {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
